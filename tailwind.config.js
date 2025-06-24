07f6cd760a661c002b10eb7f55db350ef2493fef
Jose Salazar <joesalaz46@gmail.com>
2025-06-23 21:04:47 -0500

    Updated browserslist, added new fonts to Tailwind config and layout, and applied fonts to dashboard and landing pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./views/**/*.hbs",
-    "./public/**/*.js"
-  ],
+  content: ["./views/**/*.hbs", "./public/**/*.js"],
   theme: {
     extend: {
-      colors:{
-        titlleColor:'#1c398e',
-        iconColor: '#2563eb',
-        welcomeColor: '#1c398e !important'
+      colors: {
+        titlleColor: "#1c398e",
+        iconColor: "#2563eb",
+        welcomeColor: "#1c398e !important",
       },
-      fontFamily:{
-        monserrat: ['Monserrat', 'sans-serif'],
-        playfair: ['Playfair Display', 'serif'],
-        poppins: ['Poppins', 'sans-serif'],
-        merriweather: ['Merriweather', 'serif'],
-        inter: ['Inter', 'sans-serif'],
-        roboto: ['Roboto', 'sans-serif'],
-        nunito: ['Nunito', 'sans-serif'],
-        raleway: ['Raleway', 'sans-serif'],
-        lato: ['Lato', 'sans-serif'],
-        quicksand: ['Quicksand', 'sans-serif'],
-        sourcesans: ['Source Sans 3', 'sans-serif'],
-        barlow: ['Barlow', 'sans-serif'],
-      }
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        playfair: ["Playfair Display", "serif"],
+        poppins: ["Poppins", "sans-serif"],
+        merriweather: ["Merriweather", "serif"],
+        inter: ["Inter", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+        nunito: ["Nunito", "sans-serif"],
+        raleway: ["Raleway", "sans-serif"],
+        lato: ["Lato", "sans-serif"],
+        quicksand: ["Quicksand", "sans-serif"],
+        sourcesans: ["Source Sans 3", "sans-serif"],
+        barlow: ["Barlow", "sans-serif"],
+        marhey: ["Marhey", "cursive"],
+        baloo2: ["Baloo 2", "cursive"],
+      },
     },
   },
   plugins: [],
-}
+};
 
