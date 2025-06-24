4454db8001ce4c8a0fc37c9ec89f79ccd52ee9c5
Jose Salazar <joesalaz46@gmail.com>
2025-06-23 23:48:53 -0500

    Configured express-session with SESSION_SECRET, updated package.json dependencies, and documented environment variable setup for Render deployment
    - Added express-session to dependencies
    - Refactored session middleware placement in app.js for best practices
    - Documented environment variable usage and session configuration in project setup HTML
    - Prepared codebase for secure deployment on Render

--- app.js
+++ app.js
@@ -1,54 +1,65 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var dashboardRouter = require('./routes/dashboard');
-var bookRouter = require('./routes/book');
-var booksRouter = require('./routes/books');
-const hbs = require('hbs');
-hbs.registerPartials(__dirname + '/views/partials');
-hbs.registerPartials(__dirname + '/views/layouts');
 
+
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var dashboardRouter = require("./routes/dashboard");
+var bookRouter = require("./routes/book");
+var booksRouter = require("./routes/books");
+const hbs = require("hbs");
+hbs.registerPartials(__dirname + "/views/partials");
+hbs.registerPartials(__dirname + "/views/layouts");
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.set('view options', { layout: 'layouts/layout' });
+//Initialize express session
+const session = require("express-session");
 
-app.use(logger('dev'));
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.set("view options", { layout: "layouts/layout" });
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/dashboard', dashboardRouter);
-app.use('/book', bookRouter);
-app.use('/books', booksRouter);
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET, // Usinf my own secret key
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
-
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/dashboard", dashboardRouter);
+app.use("/book", bookRouter);
+app.use("/books", booksRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
